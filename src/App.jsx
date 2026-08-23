4e5f9954a342b2a4fc841fd215716a61e8aa64c1
Djavan <dejotadeveloper@gmail.com>
2026-08-23 12:41:03 -0300

    Adicionando Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import { useEffect, useState } from 'react'
 import SplashScreen from './components/SplashScreen';
 import { AnimatePresence } from "framer-motion";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
 
